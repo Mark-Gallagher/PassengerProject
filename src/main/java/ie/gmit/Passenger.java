7a673b1c42d439804b245b763ad23973169ad2a8
Mark-Gallagher <G00343007@gmit.ie>
2022-02-09 11:14:10 +0000

    Second Commit

--- src/main/java/ie/gmit/Passenger.java
+++ src/main/java/ie/gmit/Passenger.java
@@ -4,12 +4,13 @@ public class Passenger {
 
     private String title;
     private String name;
-    private int id;
-    private int phone;
+    private String id;
+    private String phone;
     private int age;
 
-    public Passenger(String title, String name, String id, long phone, int age) {
+    public Passenger(String title, String name, String id, String phone, int age) {
        setTitle(title);
+       setName(name);
     }
 
     public String getTitle() {
@@ -17,15 +18,24 @@ public class Passenger {
     }
 
     public void setTitle(String title) {
-        if(title == "Mr" || title == "Mrs" || title == "Ms"){
+        if (title == "Mr" || title == "Mrs" || title == "Ms") {
             this.title = title;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Invalid title Entered!");
         }
 
     }
 
+    public String setName(String name) {
+        if(name.length() <= 3){
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid title Entered!");
+        }
+
+        return this.name;
+    }
 
 }
 
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -10,14 +10,29 @@ public class PassengerTest{
     @Test
     public void testTitle(){
 
-        pass = new Passenger("Mr" , "Mark", "6138294173", 1943678145l,24);
+        pass = new Passenger("Mr" , "Mark", "6138294173", "1943678145",24);
         assertEquals( "Mr", pass.getTitle());
 
     }
+//    @Test
+//    public void testName(){
+//
+//        pass = new Passenger("Mr" , "Mark", "6138294173", "1943678145",24);
+//        assertEquals( "Mark", pass.setName("Paul"));
+//
+//    }
+    @Test
     void testConstructorInvalidTitle(){
         final String invalid = "Invalid title entered";
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss", "Mary","5172839645", 1945882691l,35));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Mrs", "Mary","5172839645", "1945882691",35));
         assertEquals(invalid,exception.getMessage());
     }
+
+//    @Test
+//    void testConstructorInvalidName(){
+//        final String invalid = "Invalid name entered";
+//        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss", "Paul","5172839645", "1945882691",35));
+//        assertEquals(invalid,exception.getMessage());
+//    }
 
 }
