e12b51b0544355fed648a52f40c308cc39b6198a
Mark-Gallagher <92432812+Mark-Gallagher@users.noreply.github.com>
2022-03-12 12:34:35 +0000

    Update Passenger.java

--- src/main/java/ie/gmit/Passenger.java
+++ src/main/java/ie/gmit/Passenger.java
@@ -9,74 +9,76 @@ public class Passenger {
     private int age;
 
     public Passenger(String title, String name, String id, String phone, int age) {
-       setTitle(title);
-       setName(name);
-       setId(id);
-       setPhone(phone);
-       setAge(age);
-   }
+        setTitle(title);
+        setName(name);
+        setId(id);
+        setPhone(phone);
+        setAge(age);
+    }
 
     public String getTitle() {
         return title;
     }
 
     public String getName() {
-        return name; 
+        return name;
     }
 
-    public String getId(){
+    public String getId() {
         return id;
     }
 
     public String getPhone() {
         return phone;
     }
-    public String getPhone() {
-        return phone;
+
+    public int getAge() {
+        return age;
     }
 
     public void setTitle(String title) {
-        if (title == "Mr" || title == "Mrs" || title == "Ms") {
+        if (title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")) {
             this.title = title;
         } else {
-            throw new IllegalArgumentException("Invalid title Entered!");
+            String message = "Invalid title Entered!";
+            throw new IllegalArgumentException(message);
         }
     }
 
     public void setName(String name) {
-        if(name.length() >= 3){
+        if (name.length() >= 3) {
             this.name = name;
-        }
-        else{
-            throw new IllegalArgumentException("Invalid name Entered!");
+        } else {
+            String message = "Invalid name Entered!";
+            throw new IllegalArgumentException(message);
         }
     }
 
     public void setId(String id) {
-        if(id.length() >= 10){
+        if (id.length() >= 10) {
             this.id = id;
-        }
-        else{
-            throw new IllegalArgumentException("Invalid Id number Entered!");
+        } else {
+            String message = "Invalid id number Entered!";
+            throw new IllegalArgumentException(message);
         }
     }
-    
+
     private void setPhone(String phone) {
-        if(phone.length() >= 7){
+        if (phone.length() >= 7) {
             this.phone = phone;
-        }
-        else{
-            throw new IllegalArgumentException("Invalid Phone number Entered!");
+        } else {
+            String message = "Invalid phone number Entered!";
+            throw new IllegalArgumentException(message);
         }
     }
-    
-    private void setPhone(String age) {
-        if(age >= 16){
-            this.age= age;
+
+    private void setAge(int age) {
+        if (age >= 16) {
+            this.age = age;
+        } else {
+            String message = "Invalid age Entered!";
+            throw new IllegalArgumentException(message);
         }
-        else{
-            throw new IllegalArgumentException("Invalid age Entered!");
-        }
-    }  
+    }
 }
 
